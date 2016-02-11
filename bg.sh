d1b9108c522244dfe708ebfb52e183eb70a1756c
ambiso <ambisotopy@gmail.com>
2016-02-11 14:48:18 +0100

    Fixed error with larger sizes

--- bg.sh
+++ bg.sh
@@ -2,7 +2,7 @@
 set -ue
 #CONFIGURATION
 workdir="$HOME/.bg/" #EMPTY working directory
-tiles=4
+tiles=4 #Size of image: 1 2 4 8 16
 url="http://himawari8-dl.nict.go.jp/himawari8/img/D531106/${tiles}d/550/"
 delay=20
 outputfile="${workdir}final.png"
@@ -18,7 +18,7 @@ function cleanup {
     echo "Cleanup"
     rm -f $(for((x=0;x<$tiles;x++)); do
                 for((y=0;y<$tiles;y++)); do
-                    echo "$workdir$x$y.png"; done; done)
+                    echo "$workdir${x}_${y}.png"; done; done)
 }
 
 [ -d "$workdir" ] || (mkdir "$workdir" && echo "Created $workdir")
@@ -29,7 +29,7 @@ echo "Download"
 url="${url}$(TZ='GMT' date -d "$delay minutes ago" "+%G/%m/%d/%H")$(printf '%02d' $(echo -e a=$(TZ='GMT' date -d "$delay minutes ago" '+%M') '\na-a%10' | bc))00"
 for((x=0;x<$tiles;x++)); do 
     for((y=0;y<$tiles;y++)); do 
-        echo "${url}_${x}_${y}.png -q -O" "$workdir$x$y.png"; 
+        echo "${url}_${x}_${y}.png -q -O" "$workdir${x}_${y}.png"; 
     done; 
 done | xargs -P 32 -n 4 wget || (echo "Failed to download images"; exit 1)
 
@@ -37,7 +37,7 @@ echo "Merge"
 montage -tile ${tiles} -geometry +0+0 $(
     for((y=0;y<$tiles;y++)); do
         for((x=0;x<$tiles;x++)); do
-            echo "$workdir$x$y.png"; done; done) "${outputfile}"
+            echo "$workdir${x}_${y}.png"; done; done) "${outputfile}"
 
 cleanup
 
